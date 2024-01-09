cde04e00f8b538e70563545ed380cd8f832bca94
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-09 11:57:45 -0500

    using the input field

--- Day_1/main.py
+++ Day_1/main.py
@@ -1,5 +1,11 @@
+# Printing the single line of string
 print("Hello world")
 
+# Printing the multiple lines 
 print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
 
+# Printing the two different words
 print("Hello" + " jatt")
+
+#Printing the string with input
+print("Hello" + input("what is your name"))

ae6c576936e8c24044b5b7cf8dc4fe03c254c756
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-09 13:01:57 -0500

    Project

---
+++ Day_1/project.py
@@ -0,0 +1,4 @@
+print("welcome to the band name creator")
+city_name = input("what is the name of your city\n")
+pet_name = input("what is the name of your pet\n")
+print("name for your band could be \n" + city_name + " " + pet_name)

ab6ab156b5fd2a5acb0a5a1b28e8146cb9c34973
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-09 12:22:47 -0500

    variables

---
+++ Day_1/variable.py
@@ -0,0 +1,15 @@
+#using the variables
+name = input("what is your name")
+lenght = len(name)
+print(lenght)
+
+# Swithing the values of the variables
+a = input("value for a")
+b = input("value for b")
+
+c = a
+a = b
+b = c
+
+print(a)
+print(b)

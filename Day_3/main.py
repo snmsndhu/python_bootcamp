e6c41ec9d81ce33cbeb36081db1ffe88cf93c427
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-10 07:55:52 -0500

    Day 3 conditional statements

--- Day_3/main.py
+++ Day_3/main.py
@@ -2,3 +2,11 @@
 
 #if/else Statement
 
+print("Welcome to the rollercoaster")
+
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster")
+else:
+    print("Sorry, you can't have a ride")

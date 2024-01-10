b94e730ee457a90ed2ed919f34d7d9388f29706f
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-10 11:56:41 -0500

    pizza excerise

---
+++ Day_3/exercise2.py
@@ -0,0 +1,6 @@
+#Building a simple pizza making app
+
+print("Thank you for choosing Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L")
+add_pepperoni = input("Do you want pepperoni? Y or N")
+extra_cheese = input("Do you want extra cheese? Y or N")

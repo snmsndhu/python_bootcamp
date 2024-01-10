850081c760aebfc8935f19fc3dcef8f337e40cbc
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-10 06:45:10 -0500

    Creating a Body mass calculator

---
+++ Day_2/BMI.py
@@ -0,0 +1,8 @@
+#Creating a Body Mass calculator
+
+height = input("what is your height ")
+new_height = float(height)
+weight = input("what is your weight ")
+new_weight = float(weight)
+
+BMI = print(new_weight / (new_height ** 2))
--- Day_2/practice.py
+++ Day_2/practice.py
@@ -12,3 +12,21 @@ number = 35
 new_number = str(number)
 
 print(int(new_number[0]) + int(new_number[1]))
+
+
+#Mathematical Operations
+
+print(3 + 5)
+print(7 - 4)
+print(3 * 2)
+print(6 / 2)
+print(2 ** 6)
+
+#PEMDAS
+
+#Parentheses ()
+#Exponents **
+#Multiplication *
+#Division /
+#Addition +
+#Subtraction -

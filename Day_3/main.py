59f1c9c2b4655203c48a36c20ca36c0119b843ef
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-10 10:27:21 -0500

    excerise

--- Day_3/main.py
+++ Day_3/main.py
@@ -1,38 +1,38 @@
 #Conditional Statements, Logical Operators, Code Blocks and Scope
 
-#if/else Statement
+# if/else Statement
 
-# print("Welcome to the rollercoaster")
+print("Welcome to the rollercoaster")
 
-# height = int(input("What is your height in cm? "))
+height = int(input("What is your height in cm? "))
 
-# if height >= 120:
-#     print("You can ride the rollercoaster")
-# else:
-#     print("Sorry, you can't have a ride")
-
-
-# number = int(input("Write a number "))
+if height >= 120:
+    print("You can ride the rollercoaster")
+else:
+    print("Sorry, you can't have a ride")
 
 
-# if number % 2 == 0:
-#     print("This is the even number ")
-# else:
-#     print("This is the odd number ")
+number = int(input("Write a number "))
+
+
+if number % 2 == 0:
+    print("This is the even number ")
+else:
+    print("This is the odd number ")
 
 
 #Nested if/else statement
     
-# number = int(input("what is your height"))
+number = int(input("what is your height"))
 
-# if number >= 120:
-#     age = int(input("what is your age"))
-#     if age >= 10:
-#         print("you can take the ride")
-#     else:
-#         print("you can't take the ride")
-# else:
-#     print("you can't take the ride")
+if number >= 120:
+    age = int(input("what is your age"))
+    if age >= 10:
+        print("you can take the ride")
+    else:
+        print("you can't take the ride")
+else:
+    print("you can't take the ride")
 
 #elif condition
 number = int(input("What is your age"))

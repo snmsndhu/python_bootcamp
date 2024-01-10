154abe9fbe80578d72bad9832acdbe0ac56086d5
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-10 06:25:49 -0500

    Data types

--- Day_2/practice.py
+++ Day_2/practice.py
@@ -1,3 +1,14 @@
+#Converting a Interger into a string
+
 string = len(input("what is your name"))
 new_string = str(string)
 print("Your name has " + new_string + " char")
+
+
+#Converting a string into a interger
+
+number = 35
+
+new_number = str(number)
+
+print(int(new_number[0]) + int(new_number[1]))

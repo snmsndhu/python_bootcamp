e90000396b923ceabaee93e7e7e6fac5f321cea0
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-11 11:11:04 -0500

    project rock paper scissors

--- Day_4/project.py
+++ Day_4/project.py
@@ -5,26 +5,26 @@ game = ["ROCK", "PAPER", "SCISSORS"]
 game_length = len(game)
 
 player = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for scissors. \n "))
-
-print(f"you choose {game[player]}")
-
-
-computer = random.randint(1,2)
-print(f"computer choose {game[computer]}")
-
-
-if player > game_length:
-    print("wrong number")
-elif player == computer:
-    print("Draw")
-elif player >= 3 or player < 0 :
+if player >= 3 or player < 0 :
     print("You Lose")
-elif player == 0 and computer == 1:
-    print("You lose")
-elif player == 1 and computer == 2:
-    print("You lose")
-elif player == 2 and computer == 0:
-    print("You lose")
 else:
-    print("You won")
+    print(f"you choose {game[player]}")
+
+
+    computer = random.randint(1,2)
+    print(f"computer choose {game[computer]}")
+
+
+
+    if player == computer:
+        print("Draw")
+
+    elif player == 0 and computer == 1:
+        print("You lose")
+    elif player == 1 and computer == 2:
+        print("You lose")
+    elif player == 2 and computer == 0:
+        print("You lose")
+    else:
+        print("You won")
     

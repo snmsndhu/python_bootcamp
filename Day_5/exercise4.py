02a5560ae8c63fdc03946fc8b0be10e34a720da2
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-11 12:17:04 -0500

    FizzBuzz exercise

---
+++ Day_5/exercise4.py
@@ -0,0 +1,13 @@
+#FizzBuzz Job interview question solution
+
+target = int(input("write your number \n"))
+for number in range(1, target + 1):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    
+    else:
+        print(number)

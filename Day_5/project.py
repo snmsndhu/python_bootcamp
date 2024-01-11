de1e551a47545af63a88f365273c9ee8a2ded531
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-11 12:22:34 -0500

    project password generator

---
+++ Day_5/project.py
@@ -0,0 +1,7 @@
+#Final project of the day
+#PyPassword Generator
+
+print("Welcome to the PyPassword Generator!")
+
+total_letters = int(input("How many letters would you like in your password?"))
+

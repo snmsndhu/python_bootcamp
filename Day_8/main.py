b8b08da52473a064191cb2be92198e88ff9df65e
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-12 13:14:47 -0500

    Functions with the Arguments and the Parameters

--- Day_8/main.py
+++ Day_8/main.py
@@ -7,10 +7,15 @@ def greet():
 
 greet()
 
+
+#Arguments & Parameters
+
+#So how this function is working 
+#it is taking an parameter in the function and when we are calling this function we are passing an argument 
+#in the space of Parameter
+
 def greet_with_argument(name):
     print(f"Hello {name}")
     print("How are you")
 
 greet_with_argument("j")
-#Arguments & Parameters
-

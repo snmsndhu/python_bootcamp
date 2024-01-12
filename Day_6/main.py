5376da2366a111427a448cd3b14fe9c9c891bf52
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-12 06:09:02 -0500

    function

--- Day_6/main.py
+++ Day_6/main.py
@@ -2,3 +2,17 @@
 
 #Functions
 
+#We have used lot of buildin function of the pythons =>
+
+# print()
+# len()
+# int()
+# input()
+
+#How we can make our own function in python =>
+
+def my_function():
+    print("hello")
+    print("bye")
+
+my_function()

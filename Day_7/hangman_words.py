43c9c318e5f8211f9c6cab1e1fea4a19405f54e5
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-12 12:43:41 -0500

    Hangman Game

---
+++ Day_7/hangman_words.py
@@ -0,0 +1,12 @@
+word_list = [
+    "apple", "banana", "orange", "grape", "strawberry",
+    "computer", "keyboard", "mouse", "monitor", "printer",
+    "dog", "cat", "bird", "fish", "rabbit",
+    "house", "car", "bicycle", "book", "pen",
+    "sun", "moon", "star", "planet", "galaxy",
+    "water", "fire", "earth", "air", "energy",
+    "happy", "sad", "angry", "surprised", "confused",
+    "love", "hate", "friend", "enemy", "family",
+    "mountain", "ocean", "river", "forest", "desert",
+    "red", "blue", "green", "yellow", "purple"
+]

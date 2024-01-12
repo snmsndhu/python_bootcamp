a727bf7a8846ea95e9533f15b75f68d62f8269ba
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-12 08:36:48 -0500

    Hangman Game

--- Day_7/project.py
+++ Day_7/project.py
@@ -6,11 +6,16 @@ word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
+display = []
+
+for _ in chosen_word:
+    display += "_"
+print(display)
 guess = input("Guess your letter \n").lower()
 
 
-for world in chosen_word:
-    if world == guess:
+for word in chosen_word:
+    if word == guess:
         print("Right")
     else:
         print("wrong")

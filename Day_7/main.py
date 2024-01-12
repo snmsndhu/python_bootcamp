e1b1cfe7a2afd6bf8678ece8661be8f19d4f4f65
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-12 07:35:07 -0500

    Hangman Game

---
+++ Day_7/main.py
@@ -0,0 +1,13 @@
+#Handman Game
+
+#We are going to Build a Handman Game. It will help to review the whole week learning.
+
+
+#It will cover
+
+#For & While Loops
+#IF/ ELSE
+#Lists
+#Strings
+#Range
+#Modules

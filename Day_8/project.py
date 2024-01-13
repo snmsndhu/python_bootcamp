8f766c5336ed0c6e686b383aac68bc1a209e70b5
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-13 07:17:34 -0500

    Function project

--- Day_8/project.py
+++ Day_8/project.py
@@ -6,3 +6,17 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 direction = input("Type 'encode' to encrypt, type 'decode to decrypt: \n")
 text = input("Type your message: \n").lower()
 shift = int(input("Type the shift number: \n"))
+
+
+
+def encrypt(plain_text, shift_amount):
+  encrypted_message = ""
+  for char in plain_text:
+    position = letters.index(char)
+    new_position = position + shift_amount
+    new_letter = letters[new_position]
+    encrypted_message += new_letter
+  print(f"The encoded text is {encrypted_message}")
+    
+
+encrypt(text, shift)

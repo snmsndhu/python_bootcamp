50c1bc686a83cbcb2a0ffb764df46b8d2d5cd758
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-13 08:42:31 -0500

    Function project

--- Day_8/project.py
+++ Day_8/project.py
@@ -8,12 +8,6 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
-direction = input("Type 'encode' to encrypt, type 'decode to decrypt: \n")
-text = input("Type your message: \n").lower()
-shift = int(input("Type the shift number: \n"))
-
-shift = shift % 26
-
 def caesar(plain_text, shift_amount, direction_selection):
   end_message = ""
   if direction_selection == "decode":
@@ -29,5 +23,15 @@ def caesar(plain_text, shift_amount, direction_selection):
 
   
   
-caesar(text, shift, direction)
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode to decrypt: \n")
+    text = input("Type your message: \n").lower()
+    shift = int(input("Type the shift number: \n"))
+    shift = shift % 26
+    caesar(text, shift, direction)
+    result = input("Type 'yes' if you want to go again. Otherwise type 'no'. \n")
+    if result == "no":
+       should_continue = False
+       print("GoodBye")
      

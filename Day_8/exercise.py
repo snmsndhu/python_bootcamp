19de67e8f030281293d8b828b831f9facac8f6a8
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-13 06:14:48 -0500

    Functions exercise

--- Day_8/exercise.py
+++ Day_8/exercise.py
@@ -1,7 +1,8 @@
 #Function to calculate the cans for painting the wall
+import math
 
 def can_calculator(height, width):
     number_of_cans = (height * width) / 5
-    print(f"You will need {round(number_of_cans)} of cans")
+    print(f"You will need {math.ceil(number_of_cans)} of cans")
 
-can_calculator(2,4)
+can_calculator(3,9)

a81a7ae9ff9dcf37d9b00751b1ad2b9a0222c885
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-14 13:08:02 -0500

    Blackjack Capstone Project

--- Day_11/project.py
+++ Day_11/project.py
@@ -7,13 +7,6 @@ def deal_card():
     card = random.choice(cards)
     return card
 
-user_cards = []
-computer_cards = []
-
-for _ in range(2):
-   user_cards.append(deal_card())
-   computer_cards.append(deal_card())
-
 def calculate_score(cards):
 
     if sum(cards) == 21 and len(cards) == 2 :
@@ -24,3 +17,18 @@ def calculate_score(cards):
         cards.append(1)
 
     return sum(cards)
+
+user_cards = []
+computer_cards = []
+is_game_over = False
+
+for _ in range(2):
+   user_cards.append(deal_card())
+   computer_cards.append(deal_card())
+
+
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
+
+if user_score == 0 or computer_score == 0 or user_score > 21:
+    is_game_over = True

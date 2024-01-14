4c53ad710bc728875d4fcd3aaeb29352f5d49fd2
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-13 20:27:36 -0500

    Dictionary exercise 2

--- Day_9/exercise2.py
+++ Day_9/exercise2.py
@@ -1,6 +1,6 @@
-country = input("Country Name") 
-visits = int(input("How many times you have visited")) 
-list_of_cities = eval(input("Number of cities you have visited")) 
+country = input("Country Name \n") 
+visits = int(input("How many times you have visited \n")) 
+list_of_cities = eval(input("cities you have visited")) 
 
 travel_log = [
   {
@@ -15,6 +15,15 @@ travel_log = [
   },
 ]
 
+def add_new_country(country, visits, list_of_cities):
+    new_country = {}
+    new_country["country"] = country
+    new_country["visits"] = visits
+    new_country["cities"] = list_of_cities
+    travel_log.append(new_country)
+
+
 add_new_country(country, visits, list_of_cities)
 print(f"I've been to {travel_log[2]['country']} {travel_log[2]['visits']} times.")
 print(f"My favourite city was {travel_log[2]['cities'][0]}.")
+print(travel_log)

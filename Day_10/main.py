7f6f4230b35b003aa5914bbf7c01c2287b2f3a1d
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-14 07:28:23 -0500

    Day 10 Functions with Outputs

--- Day_10/main.py
+++ Day_10/main.py
@@ -1,1 +1,13 @@
 #Functions with Outputs
+
+#To use the function with out we have to use the return statement
+
+def format_name(f_name, l_name):
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"{formated_f_name} {formated_l_name}"
+
+
+#Then we can store the function in another variable and simply use it
+
+print(format_name("john", "jatt"))

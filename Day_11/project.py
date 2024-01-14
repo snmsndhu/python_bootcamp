1756096407861f791048ed247bf05cdb1d503d7c
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-14 13:01:45 -0500

    Blackjack Capstone Project

--- Day_11/project.py
+++ Day_11/project.py
@@ -11,4 +11,16 @@ user_cards = []
 computer_cards = []
 
 for _ in range(2):
-    deal_card()
+   user_cards.append(deal_card())
+   computer_cards.append(deal_card())
+
+def calculate_score(cards):
+
+    if sum(cards) == 21 and len(cards) == 2 :
+        return 0
+    
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+
+    return sum(cards)

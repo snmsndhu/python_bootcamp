d57c48c0ebf64fab70926603aaf06858d3c8fba2
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-14 06:41:33 -0500

    Dictionary projecy

--- Day_9/project.py
+++ Day_9/project.py
@@ -3,29 +3,26 @@
 
 print("Welcome to the BID Taker")
 
-name = input("What is your name\n")
-
-bid = int(input("How much you want to bid\n"))
-
-biders = []
-
-def bid_taker(bider_name, bider_amount):
-    new_bid = {}
-    new_bid["name"] = bider_name
-    new_bid["bid"] = bider_amount
-    biders.append(new_bid)
-
-    other_user = input("If there is other users who want to Bid. Type 'Yes' or 'No'\n")
-
-    if other_user == "Yes":
-        print()
-        
 
 
-        
-    
+bids = {}
+bidding_finished = False
 
-    
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
 
-bid_taker(name, bid)
-print(biders)
+while not bidding_finished:
+    name = input("What is your name\n$")
+    price = int(input("How much you want to bid\n"))
+    bids[name] = price
+    other_user = input("If there is other users who want to Bid. Type 'yes' or 'no'\n")
+    if other_user == "no":
+        bidding_finished = True
+    elif other_user =="yes":
+        print("clear")

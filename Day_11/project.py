692185f25f46472aacb8a11f9cacffc636fff168
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-14 13:24:24 -0500

    Blackjack Capstone Project

--- Day_11/project.py
+++ Day_11/project.py
@@ -26,18 +26,18 @@ for _ in range(2):
    user_cards.append(deal_card())
    computer_cards.append(deal_card())
 
+while not is_game_over:
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
 
-user_score = calculate_score(user_cards)
-computer_score = calculate_score(computer_cards)
+    print(f"Your cards: {user_cards}, current score: {user_score}")
+    print(f" Computer's first card: {computer_cards[0]}")
 
-print(f"Your cards: {user_cards}, current score: {user_score}")
-print(f" Computer's first card: {computer_cards[0]}")
-
-if user_score == 0 or computer_score == 0 or user_score > 21:
-    is_game_over = True
-else:
-    user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
-    if user_should_deal == "y":
-        user_cards.append(deal_card())
-    else:
+    if user_score == 0 or computer_score == 0 or user_score > 21:
         is_game_over = True
+    else:
+        user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
+        if user_should_deal == "y":
+            user_cards.append(deal_card())
+        else:
+            is_game_over = True

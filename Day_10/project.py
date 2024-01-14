b88ad54921aed78acdd61613e00473559ad58e26
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-14 08:49:33 -0500

    project

--- Day_10/project.py
+++ Day_10/project.py
@@ -21,23 +21,27 @@ operation = {
     "/": div,
 }
 
-num1 = int(input("What is the first number \n"))
+def calculaor():
+    num1 = float(input("What is the first number \n"))
 
-for key in operation:
-    print(key)
-should_continue = True
-while should_continue:
-    opt = input("What you want to do \n")
-    num2 = int(input("What is the next number\n"))
+    for key in operation:
+        print(key)
+    should_continue = True
+    while should_continue:
+        opt = input("What you want to do \n")
+        num2 = float(input("What is the next number\n"))
 
-    operation_chosen = operation[opt]
+        operation_chosen = operation[opt]
 
-    result = operation_chosen(num1, num2)
+        result = operation_chosen(num1, num2)
 
-    print(f"{num1} {opt} {num2} = {result}")
+        print(f"{num1} {opt} {num2} = {result}")
 
-    if input(f"Type 'y' to continue or type 'n' to exit:\n") == "y":
-        num1 = result
-    else:
-        should_continue = False
-        print("Thanks for using the py cal")
+        if input(f"Type 'y' to continue or type 'n' to exit:\n") == "y":
+            num1 = result
+        else:
+            should_continue = False
+            calculaor()
+            print("Thanks for using the py cal")
+
+calculaor()

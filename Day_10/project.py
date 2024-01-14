ae97e51b8d6de5488df3e9483a1d9b5d82854549
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-14 08:26:21 -0500

    project

--- Day_10/project.py
+++ Day_10/project.py
@@ -2,7 +2,7 @@
 
 print("Welcome to the py cal")
 
-def sum(n1, n2):
+def add(n1, n2):
     return n1 + n2
 
 def sub(n1, n2):
@@ -13,3 +13,24 @@ def mul(n1, n2):
 
 def div(n1, n2):
     return n1 / n2
+
+operation = {
+    "+": add,
+    "-": sub,
+    "*": mul,
+    "/": div,
+}
+
+num1 = int(input("What is the first number \n"))
+num2 = int(input("What is the second number\n"))
+
+for key in operation:
+    print(key)
+
+opt = input("What you want to do \n")
+
+operation_chosen = operation[opt]
+
+result = operation_chosen(num1, num2)
+
+print(f"{num1} {opt} {num2} = {result}")

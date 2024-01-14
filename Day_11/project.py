bba5a25a2d500a15eb6159284fe8753783cda59e
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-14 13:20:06 -0500

    Blackjack Capstone Project

--- Day_11/project.py
+++ Day_11/project.py
@@ -35,3 +35,9 @@ print(f" Computer's first card: {computer_cards[0]}")
 
 if user_score == 0 or computer_score == 0 or user_score > 21:
     is_game_over = True
+else:
+    user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
+    if user_should_deal == "y":
+        user_cards.append(deal_card())
+    else:
+        is_game_over = True

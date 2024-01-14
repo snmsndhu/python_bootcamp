f681047bf94d16a19df4a60b5a5ff4bcd962b4c3
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-14 08:06:58 -0500

    project

---
+++ Day_10/project.py
@@ -0,0 +1,15 @@
+#Calculator App
+
+print("Welcome to the py cal")
+
+def sum(n1, n2):
+    return n1 + n2
+
+def sub(n1, n2):
+    return n1 - n2
+
+def mul(n1, n2):
+    return n1 * n2
+
+def div(n1, n2):
+    return n1 / n2

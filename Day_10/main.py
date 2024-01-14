cfdced934d5d3b35afc2ae5e6abc8bd2d5ba9f11
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-14 07:36:55 -0500

    Functions with return statements

--- Day_10/main.py
+++ Day_10/main.py
@@ -11,3 +11,19 @@ def format_name(f_name, l_name):
 #Then we can store the function in another variable and simply use it
 
 print(format_name("john", "jatt"))
+
+#What will happen with more then 1 return statement
+
+#We can have multiple return statements 
+#Whatever condition matches first and if it has the return statement
+#it will terminate the fucntion over there
+
+def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"{formated_f_name} {formated_l_name}"
+
+
+print(format_name("", ""))

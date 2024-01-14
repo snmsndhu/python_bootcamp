86a5f2d777ee0ac4b0f511d3e2970312690fb0be
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-14 12:40:47 -0500

    Blackjack Capstone Project

--- Day_11/project.py
+++ Day_11/project.py
@@ -1,1 +1,14 @@
 #Blackjack Capstone Project
+
+import random
+
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+user_cards = []
+computer_cards = []
+
+for _ in range(2):
+    deal_card()

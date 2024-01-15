a963ccc7c1e5684351bb2513bb31e9d4842110dd
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-15 12:09:50 -0500

    Debugging

--- Day_13/main.py
+++ Day_13/main.py
@@ -1,2 +1,9 @@
 #Debugging
+###### Everybody gets bugs in the coding ######
+
+#First Step to solve the problem is Describe the Problem
+
+#2 Second step to avoid Reproduction of the bug
+
+#3 Play computer and Evaluate every line of code in your mind.
 

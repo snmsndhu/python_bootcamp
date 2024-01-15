05a1b3cc75f80276ebb3a2c70169ba44908f5938
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-15 08:27:49 -0500

    Scope

--- Day_12/project.py
+++ Day_12/project.py
@@ -1,15 +1,30 @@
 #Number guessing Game
 import random
+
+easy_level_attempts = 10
+hard_level_attempts = 5
+
+def check_answer(guess, answer):
+    if guess > answer:
+        print("Too high")
+    elif guess < answer:
+        print("Too low")
+    else:
+        print(f"You got it The answer was {answer}")
+
+def set_difficulty():
+    level = input("Choose a difficulty. Type 'easy' or 'hard'")
+    if level == "easy":
+        return easy_level_attempts
+    else:
+        return hard_level_attempts
+
 print("Welcome to the number Guessing Game")
 print("I'm thinking of a number between 1 and 100")
 
-numbers_list = list(range(1, 101))
-number_gussed = ""
+answer = random.randint(1, 100)
 
+guess = int(input("Make a guess"))
 
-number_to_guess = random.choice(numbers_list)
-
-print(number_to_guess)
-# def ran_number():
-
+turns = set_difficulty()
 

1d5a0d333da377cd6de64ee32389d43b371b0da2
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-15 07:12:30 -0500

    Scope

--- Day_12/main.py
+++ Day_12/main.py
@@ -7,3 +7,19 @@
 #Local Scope
 
 #Local Scope exist within the Functions
+#We can have a access of it outside of the function
+ 
+#let see a example of local scope
+
+def print_amount():
+    amount = 2
+    print(amount)
+
+#Now if we call that function we will get the value of 2 because we print that 
+#variable inside the function
+
+#But if i try to print that variable outside of the function it will not work
+print_amount()  
+
+# print(amount)
+#It will show me error of amount undefined

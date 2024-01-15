09267a70c3cfbff9be764ee079ec8e53311ced58
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-15 07:17:54 -0500

    Scope

--- Day_12/main.py
+++ Day_12/main.py
@@ -23,3 +23,22 @@ print_amount()
 
 # print(amount)
 #It will show me error of amount undefined
+
+#Global Scope
+
+#Major difference between the local scope and the global scope is that
+#Where you define the variable
+#It will effect the availbility and functionality of the variable
+#Global Scope variable will be defined Globally, which means outside of everything
+#And it can be used inside the function the functions and in other ways as well
+
+#Lets have a example of Global Scope
+
+player_health = 10
+
+def drink_potion():
+    print(player_health)
+
+drink_potion()
+
+#It will print that global variable

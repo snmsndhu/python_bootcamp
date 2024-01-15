caeaeda19e9018b3dc1cdb8b6d6a5a36cb98264f
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-15 12:24:06 -0500

    Debugging

--- Day_13/main.py
+++ Day_13/main.py
@@ -1,9 +1,15 @@
 #Debugging
 ###### Everybody gets bugs in the coding ######
 
-#First Step to solve the problem is Describe the Problem
+#First Step to solve the problem is Describe the Problem.
 
-#2 Second step to avoid Reproduction of the bug
+#2 Second step to avoid Reproduction of the bug.
 
 #3 Play computer and Evaluate every line of code in your mind.
+
+#4 Google your error.
+
+#5 Try to use Print() to find the issue in the code.
+
+#6 Use the Debugger
 

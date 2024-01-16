83124603a72589be8ac49a24ac725ed8bc11e0fd
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-16 11:10:48 -0500

    Object Oriented Programming(OOP)

--- Day_16/main.py
+++ Day_16/main.py
@@ -6,3 +6,16 @@ Object Oriented Programming is used to make the complex code easy to understand
 and easy to write.
 All this is done by splitting all bif complez code in separate modules.
 """
+
+##How we can implement the OOP##
+
+#So an object is basically combining a some sort of Data and some sort of functionality
+#Altogether in the same thing
+"""
+An object can generate multiple versions of the same object by using the blueprint.
+In OOP we can this blueprint a CLASS.
+and individual objects that are generated from that blueprint is called Object.
+
+"""
+
+##Lets make our object using the blueprint(CLASS)

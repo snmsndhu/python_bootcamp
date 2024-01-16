6050a02784aa1edaa9092b206d4cde3c3eb71b36
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-16 08:10:28 -0500

    Coffee Machine project

--- Day_15/project.py
+++ Day_15/project.py
@@ -1,10 +1,25 @@
 #Coffee Machine project
 
 from coffee import MENU, resources
-
+total_money = 0
 on = True
+
+def resources_check(order_ingre):
+    for item in order_ingre:
+        if order_ingre[item] >= resources[item]:
+            print(f"There is not enough {item}")
+            return False
+    return True
 
 while on:
     choice = input("What would you like? (espresso/latte/cappuccino): \n")
     if choice == "off":
         on = False
+    elif choice == "report":
+        print(f"Water: {resources['water']} ml")
+        print(f"Milk: {resources['milk']} ml")
+        print(f"Coffee: {resources['coffee']} ml")
+        print(f"Money: ${total_money}")
+    else:
+        drink = MENU[choice]
+        resources_check(drink["ingredients"])

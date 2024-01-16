5babb151ca3695b5bc051358e7ca3d542e345226
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-16 06:27:12 -0500

    Lower Higher Game

--- Day_14/project.py
+++ Day_14/project.py
@@ -1,1 +1,12 @@
 # Higher or Lower Game
+
+from art import logo
+from game_data import data
+import random
+
+print(logo)
+
+account_a = random.choice(data)
+account_b = random.choice(data)
+if account_a == account_b:
+    account_b = random.choice(data)

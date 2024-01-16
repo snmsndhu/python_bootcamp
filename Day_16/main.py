2bfa74ba2723354d67b8584b7977a1edf08f36cf
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-16 11:00:48 -0500

    Object Oriented Programming(OOP)

--- Day_16/main.py
+++ Day_16/main.py
@@ -1,2 +1,8 @@
      ###### INTERMEDIATE LEVEL #########
 ###### Object Oriented Programming(OOP) #########
+
+""" 
+Object Oriented Programming is used to make the complex code easy to understand 
+and easy to write.
+All this is done by splitting all bif complez code in separate modules.
+"""

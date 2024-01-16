4f824dc60a8a6e99c01f9200a69fffab7314398f
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-16 07:54:10 -0500

    Coffee Machine project

--- Day_15/project.py
+++ Day_15/project.py
@@ -1,1 +1,10 @@
 #Coffee Machine project
+
+from coffee import MENU, resources
+
+on = True
+
+while on:
+    choice = input("What would you like? (espresso/latte/cappuccino): \n")
+    if choice == "off":
+        on = False

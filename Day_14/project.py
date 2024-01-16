10407e58b6d59c5354a145b79d9da3672a90bff4
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-16 06:29:08 -0500

    Lower Higher Game

--- Day_14/project.py
+++ Day_14/project.py
@@ -10,3 +10,8 @@ account_a = random.choice(data)
 account_b = random.choice(data)
 if account_a == account_b:
     account_b = random.choice(data)
+
+account_name = account_a["name"]
+account_des = account_a["description"]
+account_country = account_a["country"]
+print(f"{account_name}, a {account_des}, from {account_country}")

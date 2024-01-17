55bea537ddad3d8ccb59600cfbe6a5589d193af5
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-17 12:08:05 -0500

    Python Turtle

---
+++ Day_18/exercise.py
@@ -0,0 +1,9 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+
+
+
+screen = Screen()
+screen.exitonclick()
--- Day_18/main.py
+++ Day_18/main.py
@@ -1,3 +1,9 @@
 ##Turtle Graphics
 ###Tuples
 ####Importing Modules
+
+
+"""
+so how to use these modules, 
+The best answer for it throught the Documentions.
+"""

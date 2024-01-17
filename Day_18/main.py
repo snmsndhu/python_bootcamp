81a9e32fd3656a2dbdcb4714b179da6d46d7d269
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-17 12:21:55 -0500

    Python Turtle

--- Day_18/main.py
+++ Day_18/main.py
@@ -7,3 +7,15 @@
 so how to use these modules, 
 The best answer for it throught the Documentions.
 """
+
+######  Graphical User Interface ######
+
+#What is graphical user interface ?
+
+"""
+
+What Is GUI? Graphical User Interfaces, Explained
+A graphical user interface (GUI) is a digital interface
+in which a user interacts with graphical components such as
+icons, buttons, and menus.
+"""

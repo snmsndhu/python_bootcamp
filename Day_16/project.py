eab61a0f79df03059861765a8b3c106dcc4d3205
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-17 06:41:02 -0500

    Project

--- Day_16/project.py
+++ Day_16/project.py
@@ -5,6 +5,15 @@ from money_machine import MoneyMachine
 
 money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
+menu = Menu()
 
-coffee_maker.report()
-money_machine.report()
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options})")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()

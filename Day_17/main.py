64081976b584626dd1a00bbc6c59792212413142
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-17 07:00:02 -0500

    Creating Class

---
+++ Day_17/main.py
@@ -0,0 +1,16 @@
+###### Creating Classes #######
+
+#How we can create a class
+
+#Syntax looks very simple
+
+class User:
+#Note word pass is used here to pass the Indent error, it is not a part of creating class.
+    pass
+
+#That all we need to create a class
+#Now we will use our class to create a object.
+
+user_1 = User()
+
+

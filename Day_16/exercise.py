70ff5f30fff5f4ae47b006bfbf39f51304c5b171
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-17 06:01:05 -0500

    ##Python Packages

--- Day_16/exercise.py
+++ Day_16/exercise.py
@@ -1,1 +1,14 @@
 ##Python Packages
+
+from prettytable import PrettyTable
+
+
+table = PrettyTable()
+
+table.field_names = ["Pokmon Name", "Type"]
+table.add_row([
+    ["Pikachu", "Electric"],
+    ["Squirtle", "Water"],
+    ["Charmander", "Fire"]
+])
+print(table)

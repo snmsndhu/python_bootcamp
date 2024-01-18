ea0f58a287aeb1ee7a428c49c8e3869ebebf00fe
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-18 06:10:43 -0500

    import statement

--- Day_18/main.py
+++ Day_18/main.py
@@ -19,3 +19,21 @@ A graphical user interface (GUI) is a digital interface
 in which a user interacts with graphical components such as
 icons, buttons, and menus.
 """
+
+#How we can import everything from the module
+
+from turtle import *
+
+#It will import everything from the turtle
+#But this method is not used that much
+#To import the whole module we can just it do like that
+
+import turtle
+
+#this is the best way to import the whole module
+
+#or if you just want to import one single method you can just import a single item
+
+from turtle import Turtle
+
+#this is the way import a single method from a module

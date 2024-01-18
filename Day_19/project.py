186356ff19a414fad612fd1172c51fcac4fa77b6
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-18 12:43:15 -0500

    Turtle Race Exercise

--- Day_19/project.py
+++ Day_19/project.py
@@ -5,9 +5,15 @@ It will help us to get more familiar with these concepts of State and Instances.
 
 from turtle import Turtle, Screen
 
-tim = Turtle()
 screen = Screen()
 screen.setup(width=500, height=400)
-screen.textinput(title="Make your bet", prompt="Which turtle will win the race ? Enter a color:")
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race ? Enter a color:")
+color = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
 
+for turtle_index in range(0, 6):
+    tim = Turtle(shape="turtle")
+    tim.color(color[turtle_index])
+    tim.penup()
+    tim.goto(x=-230, y=y_positions[turtle_index])
 screen.exitonclick()

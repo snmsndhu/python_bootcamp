f0231b45d6b3fe4736fdcfaa7a202e3afa1a0530
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-18 06:54:47 -0500

    turtle exercise

--- Day_18/exercise3.py
+++ Day_18/exercise3.py
@@ -4,8 +4,14 @@ import random
 tim = Turtle()
 
 colours = ["red", "yellow", "blue", "green"]
+directions = [0, 90, 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
 
-
+for _ in range(200):
+    tim.color(random.choice(colours))
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
 
 screen = Screen()
 screen.exitonclick()

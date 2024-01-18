e7092e6198a7c05619273e48896154c9976bc1c0
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-18 07:15:42 -0500

    RGB color

--- Day_18/exercise3.py
+++ Day_18/exercise3.py
@@ -1,17 +1,23 @@
-from turtle import Turtle, Screen
+import turtle as t
 import random
 
-tim = Turtle()
+tim = t.Turtle()
+t.colormode(255)
 
-colours = ["red", "yellow", "blue", "green"]
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+
 directions = [0, 90, 180, 270]
 tim.pensize(15)
 tim.speed("fastest")
 
 for _ in range(200):
-    tim.color(random.choice(colours))
+    tim.color(random_color())
     tim.forward(30)
     tim.setheading(random.choice(directions))
 
-screen = Screen()
-screen.exitonclick()

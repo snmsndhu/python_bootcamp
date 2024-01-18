58ea216b7ca608ec6833a0d99f40abb05a4d5381
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-18 12:27:21 -0500

    Turtle Race Exercise

--- Day_19/exercise2.py
+++ Day_19/exercise2.py
@@ -2,3 +2,12 @@
 """
 It will help us to get more familiar with these concepts of State and Instances.
 """
+
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+screen.setup(width=500, height=400)
+screen.textinput(title="Make your bet", prompt="Which turtle will win the race ? Enter a color:")
+
+screen.exitonclick()

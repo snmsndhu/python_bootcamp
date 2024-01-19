70da57564fabd1c074ae325a6db210b156c1a36f
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-19 08:36:56 -0500

    creating snake game

--- Day_20_21/project.py
+++ Day_20_21/project.py
@@ -1,7 +1,8 @@
 #Snake Game
 
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -12,6 +13,7 @@ screen.tracer(0)
 
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -23,8 +25,11 @@ is_game_on = True
 
 while is_game_on:
     screen.update()
-    time.sleep(1)
+    time.sleep(0.1)
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 
 

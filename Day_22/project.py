5ae3c2f7e7a18ef4ca56297fa151befbeef76ee6
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-19 13:41:02 -0500

    creating the Pong Game

--- Day_22/project.py
+++ Day_22/project.py
@@ -8,20 +8,16 @@ screen.tracer(0)
 
 
 r_paddle = Paddle((350, 0))
-r_paddle = Paddle((-350, 0))
+l_paddle = Paddle((-350, 0))
 
 
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
 
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 

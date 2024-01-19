17ddec1be53311501335b9ea14fded4a1f28079c
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-19 12:06:38 -0500

    creating the Pong Game

---
+++ Day_22/project.py
@@ -0,0 +1,21 @@
+from turtle import Screen
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Welcome to the Pong Game")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()

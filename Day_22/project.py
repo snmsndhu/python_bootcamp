67c40b0f62f6f7681822d6b053f6224f7324ff2e
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-19 13:37:10 -0500

    creating the Pong Game

--- Day_22/project.py
+++ Day_22/project.py
@@ -1,5 +1,5 @@
 from turtle import Screen, Turtle
-
+from paddle import Paddle
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
@@ -7,12 +7,8 @@ screen.title("Welcome to the Pong Game")
 screen.tracer(0)
 
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 0)
+r_paddle = Paddle((350, 0))
+r_paddle = Paddle((-350, 0))
 
 
 def go_up():

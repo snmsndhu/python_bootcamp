67586ae5b04d69e0d278e78d4e0e1884b63184f2
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-19 06:13:22 -0500

    creating snake game

---
+++ Day_20, 21/main.py
@@ -0,0 +1,13 @@
+#######  Building the Snake Game #########
+
+
+#Steps to make the game
+
+
+#1 Creating a snake body.
+#2 Move the snake.
+#3 Create snake food.
+#4 Delect collision with food.
+#5 Create a scoreboard.
+#6 Detect collision with wall.
+#7 Detect collision with tail.

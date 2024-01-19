1b6fee21cb956bb724b58caef629952fcbffec8d
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-19 08:08:32 -0500

    creating snake game

--- Day_20_21/main.py
+++ Day_20_21/main.py
@@ -11,3 +11,24 @@
 #5 Create a scoreboard.
 #6 Detect collision with wall.
 #7 Detect collision with tail.
+
+
+
+"""
+In building this game we will cover a new topic which is CLASS inheritance,
+which means CLASS can inherit from another classes, which will improve 
+our OOP knowledge.
+We can inherit our Class methods in the new Class.
+
+How the syntax will look like =>
+
+class Fish(Animal):
+    def __init__(self):
+       super().__init__()
+
+Over here we are inherting from the Class Animal in the Fish Class.
+and we have to add super().__init__() inside the Class to inherit all the attribute
+and the methods of the Animal Class in the Fish Class.
+"""
+
+#We will also cover the SLICING.

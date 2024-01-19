58da11af2fb56a8bd169f4de44a4c21271d2b610
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-19 06:17:09 -0500

    creating snake game

---
+++ Day_20, 21/project.py
@@ -0,0 +1,14 @@
+#Snake Game
+
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My snake Game")
+
+
+
+
+
+screen.exitonclick()

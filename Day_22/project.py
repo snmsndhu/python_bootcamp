597ca36bf36c6912178b339099d97ff913ebbc2e
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-20 06:12:06 -0500

    creating ball element

--- Day_22/project.py
+++ Day_22/project.py
@@ -1,6 +1,8 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+import time
+
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
@@ -24,7 +26,10 @@ screen.onkey(l_paddle.go_down, "s")
 game_is_on = True
 
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
 
 
 

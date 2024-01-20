c9d273aae6b28aa3c7dc459de5b98aa856c0ba56
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-20 06:48:58 -0500

    creating ball element

--- Day_22/project.py
+++ Day_22/project.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -13,6 +14,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 
 
@@ -39,9 +41,11 @@ while game_is_on:
 
     if ball.xcor() > 380:
         ball.reset_postion()
+        scoreboard.l_point()
 
     if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
 
 
 

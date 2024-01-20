6a8f84da341502b0c60dd8caa7a49057a14eb45a
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-20 08:09:28 -0500

    Turtle crossing Capstone project

--- Day_23/car_manager.py
+++ Day_23/car_manager.py
@@ -1,6 +1,24 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
-class CarManager:
-    pass
+class CarManager():
+    def __init__(self):
+        all_cars = []
+       
+
+    def create_car(self):
+        new_car = Turtle("square")
+        new_car.shapesize(stretch_wid=2, stretch_len=1)
+        new_car.penup()
+        new_car.color(random.choice(COLORS))
+        random_y = random.randint(-250, 250)
+        new_car.setheading(180)
+        new_car.goto(300, random_y)
+
+
+
+
--- Day_23/project.py
+++ Day_23/project.py
@@ -9,6 +9,7 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 player = Player()
+car = CarManager()
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
@@ -17,6 +18,7 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car.move_car()
 
 
 

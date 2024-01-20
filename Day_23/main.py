954227d8d8b4c14c1c0cb34ee285d815f43db435
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-20 07:32:40 -0500

    Turtle crossing Capstone project

---
+++ Day_23/main.py
@@ -0,0 +1,9 @@
+##The Turtle Crossing capstone Project
+
+"""
+1. Move the turtle with keypress
+2. Create and move the cars
+3. Detect collision with car
+4. Detect when turtle reaches the other side
+5. create a scoreboard
+"""

b359b25e2d9cebb222c23bb31c6d72c1349f966d
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-20 06:30:33 -0500

    creating ball element

--- Day_22/project.py
+++ Day_22/project.py
@@ -30,11 +30,11 @@ while game_is_on:
     screen.update()
     ball.move()
 
-    if ball.ycor() > 280 or ball.ycor < -280:
+    if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() -320:
-        ball.bounce_x
+        ball.bounce_x()
 
 
 

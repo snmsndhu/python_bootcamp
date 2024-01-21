d2c04e7a1a659f42eac57a22b325e8d7a3d2f633
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-21 06:40:27 -0500

    Working with CSV Files and Analysing Data with Pandas

---
+++ Day_25/main.py
@@ -0,0 +1,9 @@
+## Working with CSV Files and Analysing Data with Pandas ##
+
+## Pandas is the one of the most popular libary for the data scientists.
+
+"""
+CSV is the very common way of representing tabula data, so data that fits into
+tables like a spreadsheet. 
+CSV stands for Comma Separated Valuse.
+"""

c9c2aef9914882344ed9b87308b588e539fbd5fc
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-21 20:16:12 -0500

    List and Dictionary Comprehensions

---
+++ Day_26/main.py
@@ -0,0 +1,9 @@
+#### List and Dictionary Comprehensions ####
+"""
+This is something which is very unique in the Python, and it will help
+us to really cut down lot of code that we have to write when we are working,
+with the List and Dictionary.
+
+So, the big topic to explore is List Comprehension.
+This is something very unique in Python.
+"""

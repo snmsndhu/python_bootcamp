7564b28c6e9dde036a482481b43e47a69d7cd8ed
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-21 20:09:16 -0500

    project

--- Day_25/project/project.py
+++ Day_25/project/project.py
@@ -13,7 +13,14 @@ guessed_states = []
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 states correct", 
                                     prompt="What's another state's name").title()
-    print(answer_state)
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
 
     if answer_state in all_states:
         guessed_states.append(answer_state)
@@ -25,4 +32,3 @@ while len(guessed_states) < 50:
         t.write(state_data.state.item())
 
 
-screen.exitonclick()

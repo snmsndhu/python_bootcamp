681440bec263686b725d6a3856a15670be296ef3
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-22 12:12:52 -0500

    Graphical User Interfaces with Tkinter and Function Arguments

--- Day_27/tkinter.py
+++ Day_27/tkinter.py
@@ -11,7 +11,8 @@ window.minsize(width=500, height=300)
 my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
 my_label.pack()
 
-
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
 
 
 window.mainloop()

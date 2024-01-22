cf63f7efb9aed464a2cefc25c5a423e50b5d7cfd
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-22 07:19:04 -0500

    Graphical User Interfaces with Tkinter and Function Arguments

---
+++ Day_27/main.py
@@ -0,0 +1,5 @@
+### Graphical User Interfaces with Tkinter and Function Arguments ###
+
+#We will also look on some advanced features of the python
+## Default Arguments , Args, Kwargs.
+#How we can Tkinter to Create GUI(Graphical User Interfaces)

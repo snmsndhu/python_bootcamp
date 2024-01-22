811be0dad8b817aa20d48c22b3cc6df662ed7854
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-21 19:46:16 -0500

    project

--- Day_25/project/project.py
+++ Day_25/project/project.py
@@ -10,11 +10,14 @@ data = pandas.read_csv("50_states.csv")
 all_states = data.state.to_list()
 
 answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name")
-
+print(answer_state)
 if answer_state in all_states:
     t = turtle.Turtle()
     t.hideturtle()
     t.penup()
     state_data = data[data.state == answer_state]
-    t.goto(state_data.x, state_data.y)
-    
+    t.goto(int(state_data.x), int(state_data.y))
+    t.write(state_data.state)
+
+
+screen.exitonclick()

ab392552dd40f11d36b79d63dfcdc7829ecf0b9f
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-21 21:18:12 -0500

    exercise

--- Day_26/Exercise_3/exercise3.py
+++ Day_26/Exercise_3/exercise3.py
@@ -3,3 +3,13 @@ Write a List Comprehension to create a new list that has common numbers from two
 different files, those files are containing the numbers only.
 """
 
+with open("file1.txt") as first_list:
+    first_content = first_list.readlines()
+
+with open("file2.txt") as second_list:
+    second_content = second_list.readlines()
+    
+
+result = [num for num in first_content if first_content[num] == second_content[num]]
+
+print(result)

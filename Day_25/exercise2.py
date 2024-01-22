bbc9383825ce12773beb40fe89db317820052949
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-21 19:07:23 -0500

    exercise

---
+++ Day_25/exercise2.py
@@ -0,0 +1,11 @@
+import pandas
+
+
+data = pandas.read_csv("squirrel_data.csv")
+grey_squirrels = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels = len(data[data["Primary Fur Color"] == "Black"])
+
+
+
+

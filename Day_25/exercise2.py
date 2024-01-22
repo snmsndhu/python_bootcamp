8966d1d2ef49733dea0eda62b89e639081947e28
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-21 19:12:01 -0500

    exercise

--- Day_25/exercise2.py
+++ Day_25/exercise2.py
@@ -6,6 +6,11 @@ grey_squirrels = len(data[data["Primary Fur Color"] == "Gray"])
 red_squirrels = len(data[data["Primary Fur Color"] == "Cinnamon"])
 black_squirrels = len(data[data["Primary Fur Color"] == "Black"])
 
+data_dict = {
+    "Fur color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrels, red_squirrels, black_squirrels]
+}
 
 
-
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")

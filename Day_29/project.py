25b33f1fff52115e47860528f7b118cbb7ffa99b
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-23 10:38:52 -0500

    Building a Password manager

--- Day_29/project.py
+++ Day_29/project.py
@@ -3,3 +3,18 @@
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+from tkinter import *
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx= 20, pady= 20)
+
+
+canvas = Canvas(width=200, height=200)
+pass_manager_img = PhotoImage(file = "logo.png")
+canvas.create_image( 100, 100, image = pass_manager_img)
+canvas.grid(column=1, row=1)
+
+
+window.mainloop()

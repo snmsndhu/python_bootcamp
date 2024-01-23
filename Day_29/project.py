67ccaa781f29a84723f7a809116b89ff9dcbb2e3
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-23 11:23:56 -0500

    Building a Password manager

--- Day_29/project.py
+++ Day_29/project.py
@@ -8,35 +8,35 @@ from tkinter import *
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx= 20, pady= 20)
+window.config(padx= 80, pady= 80)
 
 
 canvas = Canvas(width=200, height=200)
 pass_manager_img = PhotoImage(file = "logo.png")
 canvas.create_image( 100, 100, image = pass_manager_img)
-canvas.grid(column=2, row=1)
+canvas.grid(column=1, row=0)
 
 website_label = Label(text="Website:")
-website_label.grid(column=1, row= 2)
-
-website_input = Entry(width=35)
-website_input.grid(row=2, column=2, columnspan=3)
+website_label.grid(column=0, row= 1)
 
 email_label = Label(text= "Email/Username:")
-email_label.grid(column=1, row=3)
-
-email_input = Entry(width=35)
-email_input.grid(row=3, column=2, columnspan=3)
+email_label.grid(column=0, row=2)
 
 password_label = Label(text="Password:")
-password_label.grid(column=1, row=4)
+password_label.grid(column=0, row=3)
 
-password_input = Entry(width=21)
-password_input.grid(row=4, column=2)
+website_input = Entry(width=35)
+website_input.grid(row=1, column=1, columnspan=2)
+
+email_input = Entry(width=35)
+email_input.grid(row=2, column=1, columnspan=2)
+
+password_input = Entry(width=18)
+password_input.grid(row=3, column=1)
 
 generate_button = Button(text="Generate Password")
-generate_button.grid(column=3, row=4)
+generate_button.grid(column=2, row=3)
 
 add_button = Button(text="Add", width=36)
-add_button.grid(column=2, row=5)
+add_button.grid(column=1,columnspan=2, row=4)
 window.mainloop()

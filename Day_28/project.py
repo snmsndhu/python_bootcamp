0675fff1a07116527fcf8cd63d38d833bc7d9968
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-23 07:16:04 -0500

    Pomodoro app

--- Day_28/project.py
+++ Day_28/project.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -14,17 +15,13 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-def count_down(count):
-    canvas.itemconfig(timer_text, text = count)
-    if count > 0:
-        window.after(1000, count_down, count - 1)
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
-count_down(5)
 
 title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
 title_label.grid(column=1, row=0)
@@ -35,8 +32,19 @@ canvas.create_image(100, 112, image = tomato_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
 canvas.grid(column=1, row=1)
 
-count_down(5)
-start_button = Button(text="Start", highlightthickness=0)
+def count_down(count):
+    count_min = math.floor(count/60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer_text, text = f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
+def start_timer():
+    count_down(5 * 60)
+
+start_button = Button(text="Start", command=start_timer, highlightthickness=0)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset", highlightthickness=0)

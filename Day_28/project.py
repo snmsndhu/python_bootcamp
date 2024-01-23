6eca53d932b7f0284e72603060f709b3f71d96ba
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-23 06:31:39 -0500

    Pomodoro app

--- Day_28/project.py
+++ Day_28/project.py
@@ -22,14 +22,19 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224)
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file= "tomato.png")
-canvas.create_image(103, 112, image = tomato_img)
-canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
-canvas.pack()
+canvas.create_image(100, 112, image = tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
+canvas.grid(column=1, row=1)
 
+start_button = Button(text="Start")
+start_button.grid(column=0, row=2)
 
-
-
+reset_button = Button(text="Start")
+reset_button.grid(column=2, row=2)
 
 window.mainloop()

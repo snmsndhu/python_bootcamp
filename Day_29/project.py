291d5d5eda9a035c3b9adf8883f13e514a3af631
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-23 11:51:42 -0500

    Building a Password manager

--- Day_29/project.py
+++ Day_29/project.py
@@ -1,7 +1,17 @@
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website_name = website_input.get()
+    email = email_input.get()
+    website_password = password_input.get()
+    with open("data.txt", mode= "a") as data_file:
+        data_file.write(f"{website_name} | {email} | {website_password} \n")
+        website_input.delete(0, END)
+        password_input.delete(0, END)
 
+
+    
 # ---------------------------- UI SETUP ------------------------------- #
 
 from tkinter import *
@@ -27,16 +37,17 @@ password_label.grid(column=0, row=3)
 
 website_input = Entry(width=35)
 website_input.grid(row=1, column=1, columnspan=2)
+website_input.focus()
 
 email_input = Entry(width=35)
 email_input.grid(row=2, column=1, columnspan=2)
-
+email_input.insert(0, "address@email.com")
 password_input = Entry(width=18)
 password_input.grid(row=3, column=1)
 
 generate_button = Button(text="Generate Password")
 generate_button.grid(column=2, row=3)
 
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command= save)
 add_button.grid(column=1,columnspan=2, row=4)
 window.mainloop()

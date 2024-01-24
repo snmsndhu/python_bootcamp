49d63e88bfb04b904f26e0358d306cd1b8e28487
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-24 06:11:50 -0500

    Errors handling

--- Day_30/main.py
+++ Day_30/main.py
@@ -16,3 +16,19 @@ Handling Erros & Exceptions
   Third line (else) will executed if there is no errors in your (try).
   Fourth line (finally) will always going to be executed. Does not matter what happens in other 3 line of code.
 """
+
+try: 
+    file = open("a_file.txt")
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["key"])
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    file.write("something")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist,")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("File was closed")

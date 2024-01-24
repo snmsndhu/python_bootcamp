dfbe6dc4869f8a0fb11fb2df6ac144b22c3f4112
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-24 07:08:15 -0500

    Errors handlingExercise

--- Day_26/Project/project.py
+++ Day_26/Project/project.py
@@ -4,8 +4,16 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 phonetic_dic = {row.letter: row.code for (index, row) in data.iterrows() }
 
-word = input("Enter a word: ").upper()
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dic[letter] for letter in word]
+    except:
+        print("Sorry, only letters in the alphabt please.")
+        generate_phonetic()
+    else:
+        print(output_list)
 
-output_list = [phonetic_dic[letter] for letter in word]
+    print(output_list)
 
-print(output_list)
+generate_phonetic()

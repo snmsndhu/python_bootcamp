e7333570f1d5c6442ad4bf0ac62259f648d78f94
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-24 09:57:24 -0500

    Flash Card App Capstone Project

--- Day_31/project.py
+++ Day_31/project.py
@@ -1,3 +1,15 @@
 #Flash Card App Capstone Project
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+from tkinter import *
+
+
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50)
+
+
+canvas = Canvas(width=800, height=526)
+
+window.mainloop()

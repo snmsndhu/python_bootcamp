a5bd642b3a77ac220f74433f0df23985966af237
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-24 05:58:26 -0500

    Errors handling

--- Day_30/main.py
+++ Day_30/main.py
@@ -1,1 +1,18 @@
 #### Errors, Exceptions and Saving JSON Data ####
+
+"""
+Handling Erros & Exceptions
+  So, how we can handle errors in the python, we have to make a error block,
+  here is the code for it =>
+  try:
+  except:
+  else:
+  finally:
+  
+  we have to fill these.
+  lets see how can make this working.
+  In first line (try) we are executing some type of code.
+  Second line (except) is the code that you want to be executed if there is exception.
+  Third line (else) will executed if there is no errors in your (try).
+  Fourth line (finally) will always going to be executed. Does not matter what happens in other 3 line of code.
+"""

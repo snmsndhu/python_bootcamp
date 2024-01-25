91fea03e42631e5692c602ff417c29f1dc396689
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-25 06:30:10 -0500

    Email SMTP and the datetime module

---
+++ Day_32/main.py
@@ -0,0 +1,11 @@
+#Email SMTP and the datetime module
+
+"""
+Basically we will learn how to send email using the python.
+
+Email SMTP is module that is pre bundle with the python and it helps us send
+email using the python.
+
+Datetime is another module, that helps us to figure out what's today date is,
+and format that.
+"""

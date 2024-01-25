0e33a3943d4220ead20f9fec4448a1e9d6fb0a5d
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-25 08:46:08 -0500

    Date module

---
+++ Day_32/exercise.py
@@ -0,0 +1,21 @@
+import smtplib
+import datetime as dt
+import random
+
+my_email = "email_address"
+password = "password"
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 1:
+    with open("quotes.txt") as quote_files:
+        all_quotes = quote_files.readlines()
+        quote = random.choice(all_quotes)
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(my_email, password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=my_email,
+        )

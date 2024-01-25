f4c5fd29e70048548c1f78398c4732cb35cdd018
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-25 07:00:29 -0500

    imp_lib

--- Day_32/project.py
+++ Day_32/project.py
@@ -3,12 +3,12 @@ import smtplib
 my_email = "email_address"
 password = "password"
 
-connection = smtplib.SMTP("smt.email.com")
-connection.starttls()
-connection.login(user=my_email, password=password)
-connection.sendmail(
-    from_addr=my_email,
-    to_addrs="rec@email.com",
-    msg= "Subject: Hello \n\nThis is the body of my email.")
+with smtplib.SMTP("smt.email.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="rec@email.com",
+        msg= "Subject: Hello \n\nThis is the body of my email.")
 
-connection.close()
+    connection.close()
--- Extras/imp_lib.py
+++ Extras/imp_lib.py
@@ -5,10 +5,21 @@ paperclip
 
 This is very useful library which we can use to auto copy something from the 
 input tab and paste it latter on.
+We have to install it.
 """
 
 """
 SMPT
 
 This is used to send automated emails
+This is preloaded
+"""
+
+
+"""
+DateTime
+
+This is the module that is used to get the current datetime.
+This is preloaded module.
+
 """

c6c0bd6a782d98c446e2ab59220abe0f63d3fb02
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-25 09:07:51 -0500

    Automated Birthday Wisher Project

--- Day_32/project/project.py
+++ Day_32/project/project.py
@@ -1,15 +1,24 @@
 #Automated Birthday Wisher Project
 
 import smtplib
+import pandas
+from datetime import datetime
 
-my_email = "email_address"
-password = "password"
+today = datetime.now()
+today_tuple = (today.month, today.day)
+
+data = pandas.read_csv("birthdays.csv")
+
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+MY_EMAIL = "email_address"
+MY_PASSWORD = "password"
 
 with smtplib.SMTP("smt.email.com") as connection:
     connection.starttls()
-    connection.login(user=my_email, password=password)
+    connection.login(user=MY_EMAIL, password=MY_PASSWORD)
     connection.sendmail(
-        from_addr=my_email,
+        from_addr=MY_EMAIL,
         to_addrs="rec@email.com",
         msg= "Subject: Hello \n\nThis is the body of my email.")
 

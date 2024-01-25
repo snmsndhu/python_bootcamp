b7b2dd9568aa5f7c83281332fd64dfdc4bba3b99
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-25 13:20:54 -0500

    Application Programming Interfaces(APIs)

--- Day_33/Exercise2/exercise2.py
+++ Day_33/Exercise2/exercise2.py
@@ -3,6 +3,7 @@ import requests
 
 def get_quote():
     response = requests.get("https://api.kanye.rest")
+    response.raise_for_status()
     data = response.json()
     canvas.itemconfig(quote_text, text = data["quote"])
     print(data["quote"])

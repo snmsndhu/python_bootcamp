7656b2b60c3c827ac6b6bf3583e2d54227451180
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-25 06:53:25 -0500

    Email SMTP and the datetime project

--- Day_32/project.py
+++ Day_32/project.py
@@ -8,8 +8,7 @@ connection.starttls()
 connection.login(user=my_email, password=password)
 connection.sendmail(
     from_addr=my_email,
-    to_addrs="rec@email.com"
-    msg= "Subject: Hello \n\nThis is the body of my email."
-)
+    to_addrs="rec@email.com",
+    msg= "Subject: Hello \n\nThis is the body of my email.")
 
 connection.close()

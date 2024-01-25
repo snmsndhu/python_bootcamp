62bcb7431aec73935fcaa91045ebe2321892e0d9
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-25 06:23:48 -0500

    Flash Card App Capstone Project

--- Day_31/project.py
+++ Day_31/project.py
@@ -37,7 +37,7 @@ def flip_card():
 def is_known():
     to_learn.remove(current_card)
     data = pandas.DataFrame(to_learn)
-    data.to_csv("data/words_to_learn.csv")
+    data.to_csv("data/words_to_learn.csv", index=False)
     next_card()
 
 window = Tk()

ab4bdb696fd30090a5f3ebffd92d6abcb5223e47
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-25 09:29:58 -0500

    python anywhere

---
+++ Extras/host.py
@@ -0,0 +1,12 @@
+#We need some kind of the platform where we can run our code everyday.
+
+#Here is it =>
+
+#----------  Python anywhere -------------#
+
+
+"""
+it does make any sense if we have to come to our local editor every single day and 
+run our code do our stuff, so we can host our code on Python anywhere and it will do this step
+for us.
+"""

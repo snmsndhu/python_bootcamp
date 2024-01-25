7d40c724bfc0e3966eb3c4eee9beac13c8fa4547
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-25 09:46:41 -0500

    Application Programming Interfaces(APIs)

---
+++ Day_33/main.py
@@ -0,0 +1,21 @@
+#Application Programming Interfaces(APIs)
+
+"""
+There are tons of website around the world, and how we can use there data in our own programs?
+
+That where APIs come in play.
+
+Application Programming Interface is a set of commands, functions, protocols, and objects that programmers,
+can use to create software or interact with an externam system.
+"""
+
+## Your program ==> API ==> External system
+
+
+"""
+So it is the set of rules to that APIs has to make a request to the external
+system for piece of data, and if you have structed your request according to thier all
+requirements that external system has set out in thier API and they will reponse to your request, 
+otherwise it will be refused.
+
+"""

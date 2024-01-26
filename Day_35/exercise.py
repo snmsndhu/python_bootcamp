29c578b9d51a2947019c7fb2254292b2df44be0a
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-26 11:27:01 -0500

    API keys, Authentication, Environment Variables and Sending SMS

--- Day_35/exercise.py
+++ Day_35/exercise.py
@@ -9,9 +9,21 @@ API_KEY = os.environ["OPEN_WEATHER_API"]
 parameters = {
     "lat": MY_LAT,
     "lon": MY_LONG,
-    "appid": API_KEY
+    "appid": API_KEY,
+    "cnt": 4
 }
 response = requests.get("https://api.openweathermap.org/data/2.5/forecast", params= parameters)
-print(response.status_code)
+response.raise_for_status()
 data = response.json()
-print(data)
+
+will_rain = False
+for hour_data in data["list"]:
+    condition_code = (hour_data["weather"][0]["id"])
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("Bring an umbrella.")
+else:
+    print("It will be not raining")
+        

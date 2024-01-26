a0936dce304d7ab82ce85c69bca9c82bd3e86780
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-26 09:54:05 -0500

    API keys, Authentication, Environment Variables and Sending SMS

---
+++ Day_35/main.py
@@ -0,0 +1,16 @@
+#API keys, Authentication, Environment Variables and Sending SMS
+
+"""
+What is API Authentication ?
+
+It is basically the APIs keys that are paid not free or we have to get authentication,
+in order to use them.
+Every Authentication has a free plan to learn about and for their sake of safety, 
+so that anybody can not use their free plan on big scale.
+
+They provide API keys.
+
+What are the API Keys ?
+
+This is most likely your personal accound number or password.
+"""

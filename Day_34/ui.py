e8cd8927439626a3e826917fb232347ee7d17992
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-26 07:09:27 -0500

    Trivia Quiz Game Using APIs

--- Day_34/ui.py
+++ Day_34/ui.py
@@ -1,2 +1,16 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+        self.canvas = Canvas(height=250, width=300)
+
+        self.window.mainloop()

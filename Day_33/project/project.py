0cdf805a46f0887a0aa7eadfde07e2bcd8d7e9f3
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-26 06:02:22 -0500

    Overhead Notifier Project

--- Day_33/project/project.py
+++ Day_33/project/project.py
@@ -1,15 +1,20 @@
 import requests
 from datetime import datetime
+import smtplib
+
 
 MY_LAT = 56.130367 # Your latitude
 MY_LONG = -106.346771 # Your longitude
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-data = response.json()
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
 
-iss_latitude = float(data["iss_position"]["latitude"])
-iss_longitude = float(data["iss_position"]["longitude"])
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+    if MY_LAT -5 <= iss_latitude <= MY_LAT-5 and MY_LAT-5 <= iss_longitude <= MY_LONG+5:
+        return True
 
 #Your position is within +5 or -5 degrees of the ISS position.
 

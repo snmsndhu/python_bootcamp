1c23117b541fe8c919b047684dd93d2ed8bfb7c7
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-27 06:44:56 -0500

    API keys project

--- Day_36/project.py
+++ Day_36/project.py
@@ -27,3 +27,12 @@ yesterday_data = data_list[0]
 yesterday_closing_price = yesterday_data["4. close"]
 print(yesterday_closing_price)
 
+day_before_yesterday_data = data_list[1]
+day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
+print(day_before_yesterday_closing_price)
+
+difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
+print(difference)
+
+diff_percent = (difference / float(yesterday_closing_price)) * 100
+print(diff_percent)

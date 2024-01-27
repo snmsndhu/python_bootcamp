68889831baabf5cace863be75352f23a97674bbc
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-27 07:34:24 -0500

    Advanced Authentication and GET/POST/PUT/DELETE Requests

---
+++ Day_37/main.py
@@ -0,0 +1,17 @@
+##Advanced Authentication and GET/POST/PUT/DELETE Requests.
+"""
+GET 
+ Is is used to get the data from the External system, and they give us the data in the response.
+ request.get()
+
+POST
+  In post request we give external system some sort of data.
+  request.post()
+
+PUT
+
+
+DELETE
+
+
+"""

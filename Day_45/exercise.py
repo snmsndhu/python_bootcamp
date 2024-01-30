681ea9853cc4a6f47bb85f601542ce5e5ec17099
Sandeep Singh <snmsndhu47@gmail.com>
2024-01-30 06:14:23 -0500

    Beautiful soup lib

---
+++ Day_45/exercise.py
@@ -0,0 +1,20 @@
+#How we can use beautiful soup on live website...
+
+
+from bs4 import BeautifulSoup
+import requests
+
+response = requests.get("https://news.ycombinator.com/news")
+yc_web_page = response.text
+
+soup = BeautifulSoup(yc_web_page, "html.parse")
+articles = soup.find_all(name="a", class_="storylink")
+article_texts = []
+article_links = []
+for article_tag in articles:
+    text = article_tag.getText()
+    article_texts.append(text)
+    link = article_tag.get("href")
+    article_links.append(link)
+
+article_upvotes = [score.getText() for score in soup.find_all(name="span", class_ = "score") ]
